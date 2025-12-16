3686390fabc5d8fe194392e9d05f5675ca5a3c5d
Shilpa <chinnagownishilpa@gmail.com>
2025-12-16 20:10:51 +0530

    Refactor ExtentManager and integrate Extent reporting via Hooks

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -7,12 +7,17 @@ public class ExtentManager {
 
     private static ExtentReports extent;
 
-    public static ExtentReports getExtent() {
+    private ExtentManager() {
+        // prevent external instantiation
+    }
+
+    public static ExtentReports getExtentReport() {
         if (extent == null) {
+
             ExtentSparkReporter reporter =
                     new ExtentSparkReporter("target/ExtentReport.html");
 
-            reporter.config().setReportName("UI Automation Test Report");
+            reporter.config().setReportName("BDD Automation Test Report");
             reporter.config().setDocumentTitle("Automation Execution Report");
 
             extent = new ExtentReports();
--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,18 +1,43 @@
 package hooks;
 
 import base.BaseTest;
+import base.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import utils.ExtentManager;
+import utils.ScreenshotUtil;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 
 public class Hooks extends BaseTest {
 
+    private static ExtentReports extent = ExtentManager.getExtentReport();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
     @Before
-    public void setUp() {
+    public void setUp(Scenario scenario) {
         setup();
+        test.set(extent.createTest(scenario.getName()));
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+
+        if (scenario.isFailed()) {
+            String screenshotPath = ScreenshotUtil.captureScreenshot(
+                    DriverFactory.getDriver(),
+                    scenario.getName().replace(" ", "_"));
+
+            test.get()
+                .fail("Scenario Failed")
+                .addScreenCaptureFromPath(screenshotPath);
+        } else {
+            test.get().pass("Scenario Passed");
+        }
+
+        extent.flush();
         tearDown();
     }
 }
